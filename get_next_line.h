6f7165bb2fa126ac3d5d42b97a20d506e8067c7e
Melanie ZHU <mzhu@e2r8p1.42.fr>
2019-11-07 06:41:54 +0100

    cleaned

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mzhu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 19:00:35 by mzhu              #+#    #+#             */
-/*   Updated: 2019/08/22 17:02:18 by mzhu             ###   ########.fr       */
+/*   Updated: 2019/11/07 06:39:43 by mzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 15
 
-# include <sys/types.h>
-# include <sys/stat.h>
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <string.h>
 # include "./libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
